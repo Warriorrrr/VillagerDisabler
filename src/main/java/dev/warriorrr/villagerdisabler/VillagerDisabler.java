5b428a9612c2dba4c55819abe1254609ed19d04b
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-05-10 18:44:49 +0200

    Use the correct event.

--- src/main/java/dev/warriorrr/villagerdisabler/VillagerDisabler.java
+++ src/main/java/dev/warriorrr/villagerdisabler/VillagerDisabler.java
@@ -1,9 +1,6 @@
 package dev.warriorrr.villagerdisabler;
 
 import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class VillagerDisabler extends JavaPlugin {
--- src/main/java/dev/warriorrr/villagerdisabler/VillagerListener.java
+++ src/main/java/dev/warriorrr/villagerdisabler/VillagerListener.java
@@ -3,12 +3,12 @@ package dev.warriorrr.villagerdisabler;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityInteractEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class VillagerListener implements Listener {
     @EventHandler(ignoreCancelled = true)
-    public void onEntityInteract(EntityInteractEvent event) {
-        if (event.getEntityType().equals(EntityType.VILLAGER))
+    public void onEntityInteract(PlayerInteractEntityEvent event) {
+        if (event.getRightClicked().getType().equals(EntityType.VILLAGER))
             event.setCancelled(true);
     }
 }
