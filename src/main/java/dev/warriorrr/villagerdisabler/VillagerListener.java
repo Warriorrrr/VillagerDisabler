70f9975884cf7ec217fdab00ace6ad09ff78dfe8
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-05-10 19:17:46 +0200

    Prevent wandering traders from entering boats/minecarts

--- src/main/java/dev/warriorrr/villagerdisabler/VillagerListener.java
+++ src/main/java/dev/warriorrr/villagerdisabler/VillagerListener.java
@@ -4,11 +4,18 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
 
 public class VillagerListener implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onEntityInteract(PlayerInteractEntityEvent event) {
         if (event.getRightClicked().getType().equals(EntityType.VILLAGER))
+            event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onEnterBoat(VehicleEnterEvent event) {
+        if (event.getEntered().getType().equals(EntityType.WANDERING_TRADER))
             event.setCancelled(true);
     }
 }
